5259336f05bff76fd46ead2908beedf2b560ca58
Dams <mariotto.damien@gmail.com>
2019-01-25 10:27:52 +0100

    changes

---
+++ Front/build/precache-manifest.5fa2930d5147615b32b1bacccb2fc826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aa08876423262f868ca",
+    "url": "/static/css/main.80febb3b.chunk.css"
+  },
+  {
+    "revision": "8aa08876423262f868ca",
+    "url": "/static/js/main.8aa08876.chunk.js"
+  },
+  {
+    "revision": "8d43fb5cc0c43f4eed66",
+    "url": "/static/js/1.8d43fb5c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8f7b3d8f470fcb0b17cfb99ddac6d4a",
+    "url": "/index.html"
+  }
+];
